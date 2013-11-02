ae0da8bc43da5c58fc58499cc07fedcebc4401d9
Marcus Vinicius Pimenta <mvinicius.pimenta@gmail.com>
2013-11-01 22:10:51 -0300

    Initial commit. Setting serial in arduino

---
+++ Car Controller Arduino/car_controller/car_controller.ino
@@ -0,0 +1,25 @@
+
+int led = 13;
+
+void setup() {
+    pinMode(led, OUTPUT);
+    digitalWrite(led, HIGH);
+    
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        char inChar = Serial.read();
+        
+        switch (inChar) {   
+            case 'a':
+              digitalWrite(led, HIGH);
+              break;
+            case 'b':
+              digitalWrite(led, LOW);
+              break;
+        }
+    }
+}
+
